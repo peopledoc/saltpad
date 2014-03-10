bdfb996f551e0dd3d3217ebcd1b0e3b578f73b78
FELD Boris <lothiraldan@gmail.com>
2014-03-10 09:31:11 +0100

    Import generated boilerplate for saltpad

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Boris FELD <boris.feld@novapost.fr>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='saltpad',
+    version='0.0.1',
+    description='SaltPad is a GUI and CLI tool to manage saltstack deployments + orchestration.',
+    long_description=readme + '\n\n' + history,
+    author='Boris FELD',
+    author_email='boris.feld@novapost.fr',
+    url='https://github.com/novapost/saltpad',
+    packages=[
+        'saltpad',
+    ],
+    package_dir={'saltpad': 'saltpad'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    zip_safe=False,
+    keywords='saltpad',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
+)
